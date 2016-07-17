54c83b8b54e337427798b02dfb134861cb85c8e8
Iben Van de Veire <ibenvandeveire@gmail.com>
2016-07-17 12:13:56 +0200

    Fix
    Service

--- app/js/Controllers/BiomeController.js
+++ app/js/Controllers/BiomeController.js
@@ -1,3 +1,11 @@
 app.controller("BiomeCTRL", ["$scope", "biomes", function ($scope, biomes) {
     $scope.biomes = biomes.data;
+    $scope.currentBiome = biomes.data[0];
+    $scope.getBiome = function (id) {
+        for (var i = 0; i < biomes.data.length; i++) {
+            if (biomes.data[i].ID === id) {
+                $scope.currentBiome = biomes.data[i];
+            }
+        }
+    }
     }]);
--- app/js/route.js
+++ app/js/route.js
@@ -1,19 +1,19 @@
-            app.config(['$routeProvider', function ($routeProvider) {
-                $routeProvider.when('/', {
-                    template: 'index.html'
-                }).when('/downloads', {
-                    templateUrl: 'html/downloads.html'
-                }).when('/products', {
-                    templateUrl: 'html/products.html'
-                }).when('/biomes', {
-                    templateUrl: 'html/products/biomes.html',
-                    controller: "BiomeCTRL",
-                    resolve: {
-                        biomes: ["biomeService", function (biomeService) {
-                            return biomeService.load();
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {
+        template: 'index.html'
+    }).when('/downloads', {
+        templateUrl: 'html/downloads.html'
+    }).when('/products', {
+        templateUrl: 'html/products.html'
+    }).when('/biomes', {
+        templateUrl: 'html/products/biomes.html',
+        controller: "BiomeCTRL",
+        resolve: {
+            biomes: ["biomeService", function (biomeService) {
+                return biomeService.load();
                     }]
-                    }
-                }).otherwise({
-                    redirectTo: '/'
-                });
+        }
+    }).otherwise({
+        redirectTo: '/'
+    });
             }]);
